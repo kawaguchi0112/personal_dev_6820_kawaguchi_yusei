abc508e2f920da7f32439009e6aaceb6caf399f4
kawaguchi0112 <k.y.solxyz@gmail.com>
2024-05-15 15:41:52 +0900

    ファーストコミット

---
+++ src/main/java/com/example/demo/PersonalDev6820KawaguchiYuseiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDev6820KawaguchiYuseiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDev6820KawaguchiYuseiApplication.class, args);
+	}
+
+}
